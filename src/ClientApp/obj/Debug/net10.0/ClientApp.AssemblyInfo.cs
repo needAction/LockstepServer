ad2dd170055caa99decafd3164d3afe615895b7b
쿠키 <hollowinday@gmail.com>
2026-07-27 18:02:33 +0900

    chore: .gitignore 적용 및 빌드 임시 파일 추적 제거

--- src/ClientApp/obj/Debug/net10.0/ClientApp.AssemblyInfo.cs
+++ src/ClientApp/obj/Debug/net10.0/ClientApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5f41e4e94e71b98dda7e6458bbecf6e28c4efa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a588de95b5b21e8d6ef9b7cfa2a7b7da9eb05e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
